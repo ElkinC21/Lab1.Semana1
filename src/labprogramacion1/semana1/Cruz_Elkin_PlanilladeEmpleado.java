e72d0d0661a969e110e0c3ffeff6489e2a9e1e96
elkin <elkin@DESKTOP-KQKAO9U>
2025-04-25 17:02:38 +0200

    Carga Inicial

---
+++ src/labprogramacion1/semana1/Cruz_Elkin_PlanilladeEmpleado.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogramacion1.semana1;
+
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_PlanilladeEmpleado {    
+
+
+
+
+
+}
+
+
