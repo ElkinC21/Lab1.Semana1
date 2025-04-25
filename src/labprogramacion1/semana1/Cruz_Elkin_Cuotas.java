c26bbb30fe245a4f8af8457856188047e59d9598
elkin <elkin@DESKTOP-KQKAO9U>
2025-04-25 17:11:11 +0200

    Carga Inicial

---
+++ src/labprogramacion1/semana1/Cruz_Elkin_Cuotas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogramacion1.semana1;
+import java.util.Scanner;
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_Cuotas {
+public static void main(String[]args){    
+
+
+
+}}
