1aaca3e6054d1352023172335ffb74f33d09a3c1
elkin <elkin@DESKTOP-KQKAO9U>
2025-04-25 18:18:19 +0200

    Carga inicial ejercicio 4

---
+++ src/labprogramacion1/semana1/Cruz_Elkin_Formulas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package labprogramacion1.semana1;
+import java.util.Scanner;
+/**
+ *
+ * @author elkin
+ */
+public class Cruz_Elkin_Formulas {
+public static void main(String[]args){    
+
+
+}}
